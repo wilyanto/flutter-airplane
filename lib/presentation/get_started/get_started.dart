96c0fa8bc2cdbd2393c25d9237e80d97dc0a94a8
wilyanto <wilyanto@code.id>
2021-08-29 15:54:21 +0700

    get started

---
+++ lib/presentation/get_started/get_started.dart
@@ -0,0 +1,69 @@
+import 'package:airplane/shared/theme.dart';
+import 'package:flutter/material.dart';
+
+class GetStartedPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            width: double.infinity,
+            height: double.infinity,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(
+                  'assets/image_get_started.png',
+                ),
+              ),
+            ),
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text(
+                  'Fly Like a Bird',
+                  style: whiteTextStyle.copyWith(
+                      fontSize: 32, fontWeight: semiBold),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  'Explore new world with us and let yourself get an amazing experiences',
+                  style: whiteTextStyle.copyWith(
+                    fontSize: 16,
+                    fontWeight: light,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 50, bottom: 80),
+                  width: 200,
+                  height: 55,
+                  child: TextButton(
+                    onPressed: () {},
+                    style: TextButton.styleFrom(
+                      backgroundColor: kPrimaryColor,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(defaultRadius),
+                      ),
+                    ),
+                    child: Text(
+                      'Get Started',
+                      style: whiteTextStyle.copyWith(
+                        fontSize: 18,
+                        fontWeight: medium,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
