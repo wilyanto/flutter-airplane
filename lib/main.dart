551438de2d3d47ff6a77066a971f872048290a71
wilyanto <wilyanto@code.id>
2021-08-29 15:54:25 +0700

    add linter

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:airplane/presentation/get_started/get_started.dart';
 import 'package:flutter/material.dart';
 
 import 'presentation/splash/splash_page.dart';
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SplashPage(),
+      home: GetStartedPage(),
     );
   }
 }
