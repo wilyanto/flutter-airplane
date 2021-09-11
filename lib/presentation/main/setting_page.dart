7b9d602f91dcaf541a91cdd410f8da45c4283474
wilyanto <wilyanto@code.id>
2021-09-12 04:13:38 +0700

    adding trx, wallet, and setting simple page

---
+++ lib/presentation/main/setting_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SettingPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Setting Page'),
+    );
+  }
+}
---
+++ lib/presentation/transaction/transaction_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TransactionPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Transaction Page'),
+    );
+  }
+}
